25f20041be2dd8df20caf6c520031367b0d17cec
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2022-04-29 16:08:10 +0800

    use go1.18

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	leetcodeoffer "github.com/havardzzl/havardzzl/leetcode-offer"
-)
+func gMap[T1 any, T2 any](arr []T1, f func(T1) T2) []T2 {
+	result := make([]T2, len(arr))
+	for i, elem := range arr {
+		result[i] = f(elem)
+	}
+	return result
+}
 
 func main() {
-	res := &[]int{}
-	leetcodeoffer.InorderTraversal(leetcodeoffer.TestTree, res)
-	fmt.Println(*res)
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	squares := gMap(nums, func(elem int) int {
+		return elem * elem
+	})
+	fmt.Println(squares)
 }
