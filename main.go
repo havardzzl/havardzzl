8ef8d05111d20136393a86c8a0557e04ca44fe54
zhangzhongliang <zhangzhongliangzzl@gmail.com>
2022-08-27 03:00:40 +0800

    vendor

--- main.go
+++ main.go
@@ -2,33 +2,47 @@ package main
 
 import (
 	"fmt"
-	"time"
-	// "github.com/havardzzl/havardzzl/leetcodeoffer"
+	"sync"
+
+	"github.com/havardzzl/havardzzl/db"
 )
 
-type K struct {
-	B int
-	A int
-}
+var (
+	cc = make(chan int)
+	wg sync.WaitGroup
+)
 
-func (k *K) Chdl() {
-	fmt.Println("chdl")
-}
-
-func kdf(k *K) {
-	defer updateMutationElapsedTimeMetrics()()
-	time.Sleep(time.Second)
-	k.Chdl()
+func workCc(cnt int) {
+	defer wg.Done()
+	fmt.Println("I'am wait for cc: ", cnt)
+	for {
+		i, ok := <-cc
+		if !ok {
+			fmt.Println("cc closed quit")
+			return
+		}
+		fmt.Println("get from cc:", i, " I'am ", cnt)
+	}
 }
 
 func main() {
-	kdf(nil)
-}
+	db.Init()
+	db.NewTask()
+	db.Close()
 
-func updateMutationElapsedTimeMetrics() func() {
-	start := time.Now()
-	return func() {
-		end := time.Now()
-		fmt.Println("Elapsed time:", end.Sub(start))
-	}
+	// con := 3
+	// for i := 0; i < con; i++ {
+	// 	go workCc(i)
+	// }
+	// wg.Add(con)
+	// go func() {
+	// 	time.Sleep(time.Second)
+	// 	cc <- 5
+	// 	time.Sleep(time.Second * 2)
+	// 	cc <- 6
+	// 	time.Sleep(time.Second * 3)
+	// 	cc <- 7
+	// 	close(cc)
+	// }()
+	// wg.Wait()
 }
