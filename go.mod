046bd52d383c140558494ad41eb736bc5dee458c
zhangzhongliang@kanzhun.com <zhangzhongliang@kanzhun.com>
2022-12-22 17:03:40 +0800

    1. add tls probe 2. add python example 3. add vendor

--- go.mod
+++ go.mod
@@ -2,11 +2,22 @@ module github.com/havardzzl/havardzzl
 
 go 1.17
 
-require k8s.io/apimachinery v0.24.1
+require (
+	github.com/prometheus/client_golang v1.13.0
+	k8s.io/apimachinery v0.25.0
+)
 
 require (
-	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	k8s.io/klog/v2 v2.60.1 // indirect
-	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os/exec"
 	"strconv"
 	"time"
+
+	"github.com/havardzzl/havardzzl/probe"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +16,17 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("get body: " + string(bs))
 	r.Body.Close()
 	fmt.Fprintf(w, "Hello World")
+}
+
+func KubeHandler(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("kubectl", "get", "nodes")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	fmt.Fprint(w, string(output))
 }
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,8 +40,11 @@ func TimeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", HelloHandler)
-	http.HandleFunc("/time", TimeHandler)
-	fmt.Println("begin listening at :8979")
-	http.ListenAndServe(":8979", nil)
+	// http.HandleFunc("/", HelloHandler)
+	// http.HandleFunc("/kube", KubeHandler)
+	// http.HandleFunc("/time", TimeHandler)
+	// fmt.Println("begin listening at :8979")
+	// http.ListenAndServe(":8979", nil)
+
+	fmt.Println("probe result:", probe.ProbeTls())
 }
